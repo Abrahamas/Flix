8f0bbad47f3d2196a75f2b4154b71b9d1f3bf163
abraham asmile <babyyuguioh@gmail.com>
2018-09-15 17:45:35 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Mac on 6/24/1397 AP.
+//  Copyright © 1397 Abraham Asmile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
